dc476a015f500267e2f4e518fb7a99f2d0a4f83e
Sergey Khomushin <sergey@placer.io>
2020-05-06 19:27:36 +0300

    GmailTransport: refactoring: - used new API - fixed the issue with huge attachments

--- src/GmailTransport.ts
+++ src/GmailTransport.ts
@@ -10,6 +10,8 @@ type MailMessageWithBcc = MailMessage & {
   }
 };
 
+type DoneCallback = (err: Error | null, info?: SentMessageInfo) => void;
+
 interface GoAuth2 {
   access_token: string;
   token_type: string;
@@ -33,6 +35,38 @@ export interface Options {
   userId?: string;
 }
 
+function getErrorCode(error: GmailError['error']) {
+  return typeof error === 'object' && error !== null ? error.code : 0;
+}
+
+function refreshTokenParams(auth: OAuth2Options) {
+  return {
+    client_id: auth.clientId,
+    client_secret: auth.clientSecret,
+    refresh_token: auth.refreshToken,
+    grant_type: 'refresh_token'
+  };
+}
+
+function createError(ge: GmailError | string) {
+  if (!(typeof ge === 'object' && ge !== null)) return new Error(ge as string);
+
+  if (ge.error === 'invalid_grant') {
+    // return a better message instead of 'Bad Request' from the Gmail error_description.
+    return new Error('Invalid grant. Please reconnect your Gmail account');
+  }
+
+  if (ge.error_description) {
+    return new Error(ge.error_description);
+  }
+
+  if (typeof ge.error === 'object' && ge.error !== null) {
+    return new Error(ge.error.message);
+  }
+
+  return new Error(ge.error as string);
+}
+
 export class GmailTransport implements Transport {
 
   public name: string = 'GmailTransport';
@@ -40,58 +74,34 @@ export class GmailTransport implements Transport {
 
   constructor(private options: Options) {}
 
-  private messageToBase64Raw(message: Buffer): string {
-    return message.toString('base64')
-      .replace(/\+/g, '-')
-      .replace(/\//g, '_')
-      .replace(/=+$/, '');
-  }
-
-  private refreshTokenParams(auth: OAuth2Options): Object {
-    return {
-      client_id: auth.clientId,
-      client_secret: auth.clientSecret,
-      refresh_token: auth.refreshToken,
-      grant_type: 'refresh_token'
-    };
-  }
-
-  private createError(ge: GmailError): Error {
-    if (ge.error === 'invalid_grant') {
-      // return a better message instead of 'Bad Request' from Gmail error_description.
-      return new Error('Invalid grant. Please reconnect your Gmail account');
-    }
-
-    if (ge.error_description) {
-      return new Error(ge.error_description);
-    }
-
-    if (typeof ge.error === 'object' && ge.error !== null) {
-      return new Error(ge.error.message);
-    }
-
-    return new Error(<string>(ge.error || ge));
-  }
-
   private getAccessToken(): Promise<string> {
     return Requestly.post({
       protocol: 'https:',
       hostname: 'www.googleapis.com',
       path: '/oauth2/v4/token'
-    }, this.refreshTokenParams(this.options.auth))
+    }, refreshTokenParams(this.options.auth))
       .then((data: GoAuth2) => data.access_token);
   }
 
-  private sendMail(message: Buffer, accessToken: string): Promise<any> {
-    return Requestly.postJSON({
+  private post(message: Buffer, accessToken: string): Promise<any> {
+    return Requestly.postRFC822({
       protocol: 'https:',
       hostname: 'www.googleapis.com',
-      path: `/gmail/v1/users/${this.options.userId || 'me'}/messages/send`,
+      path: `/upload/gmail/v1/users/${this.options.userId || 'me'}/messages/send?uploadType=multipart`,
       headers: {
         Authorization: `Bearer ${accessToken}`
       }
-    }, {
-      raw: this.messageToBase64Raw(message)
+    }, message.toString());
+  }
+
+  private sendMail(data: Buffer, accessToken: string, mail: MailMessage, done: DoneCallback): Promise<void> {
+    return this.post(data, accessToken).then((message) => {
+      done(null, {
+        envelope: mail.message.getEnvelope(),
+        messageId: mail.message.messageId(),
+        accessToken: accessToken,
+        message: message
+      });
     });
   }
 
@@ -100,28 +110,18 @@ export class GmailTransport implements Transport {
     mail.message.build((error, data: Buffer) => {
       if (error) return done(error);
 
-      this.sendMail(data, this.options.auth.accessToken).then((message) => {
-        done(null, {
-          envelope: mail.message.getEnvelope(),
-          messageId: mail.message.messageId(),
-          accessToken: this.options.auth.accessToken,
-          message: message
-        });
-      }, (e: GmailError) => {
-        if (e.error && (e.error as GmailErrorObject).code === 401 && this.options.auth.refreshToken) {
-          this.getAccessToken().then((accessToken: string) => {
-            this.sendMail(data, accessToken).then((message) => {
-              done(null, {
-                envelope: mail.message.getEnvelope(),
-                messageId: mail.message.messageId(),
-                accessToken: accessToken,
-                message: message
-              });
-            }).catch((er: GmailError) => done(this.createError(er)));
-          }).catch((er: GmailError) => done(this.createError(er)));
-        } else {
-          done(this.createError(e));
-        }
+      this.sendMail(data, this.options.auth.accessToken, mail, done)
+        .catch((e: GmailError) => {
+
+          if (getErrorCode(e.error) === 401 && this.options.auth.refreshToken) {
+            this.getAccessToken().then((accessToken: string) => {
+              this.sendMail(data, accessToken, mail, done)
+                .catch((er: GmailError) => done(createError(er)));
+            }).catch((er: GmailError) => done(createError(er)));
+          }
+          else {
+            done(createError(e));
+          }
       });
     });
   }
