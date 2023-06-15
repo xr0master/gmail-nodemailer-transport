9f4f63c717e07f4ab13af20e404ca2cc43d3cba8
Sergey Khomushin <xr0master@gmail.com>
2023-06-15 09:52:48 +0800

    improve types

--- src/GmailTransport.ts
+++ src/GmailTransport.ts
@@ -1,51 +1,43 @@
-import type {SentMessageInfo, Transport} from 'nodemailer';
-import type {Options as OAuth2Options} from 'nodemailer/lib/xoauth2';
+import type { SentMessageInfo, Transport } from 'nodemailer';
+import type { Options as OAuth2Options } from 'nodemailer/lib/xoauth2';
 import type MailMessage from 'nodemailer/lib/mailer/mail-message';
 
-import {Requestly} from './services/Requestly';
+import { post, postRFC822 } from './services/Requestly';
 
 type MailMessageWithBcc = MailMessage & {
   message: {
-    keepBcc: boolean
-  }
+    keepBcc: boolean;
+  };
 };
 
 type DoneCallback = (err: Error | null, info?: SentMessageInfo) => void;
-
-interface GoAuth2 {
-  access_token: string;
-  token_type: string;
-  id_token: string;
-  scope: string;
-  expires_in: number;
-}
-
-interface GmailErrorObject {
-  code: number;
-  message: string;
-}
-
-interface GmailError {
-  error_description: string;
-  error: string | GmailErrorObject;
-}
 
 export interface Options {
   auth: OAuth2Options;
   userId?: string;
 }
 
-function getErrorCode(error: GmailError['error']) {
-  return typeof error === 'object' && error !== null ? error.code : 0;
-}
-
 function refreshTokenParams(auth: OAuth2Options) {
   return {
-    client_id: auth.clientId,
-    client_secret: auth.clientSecret,
-    refresh_token: auth.refreshToken,
-    grant_type: 'refresh_token'
+    client_id: auth.clientId!,
+    client_secret: auth.clientSecret!,
+    refresh_token: auth.refreshToken!,
+    grant_type: 'refresh_token',
   };
+}
+
+interface GmailError {
+  error_description: string;
+  error:
+    | {
+        code: number;
+        message: string;
+      }
+    | string;
+}
+
+function getErrorCode(error: GmailError['error']) {
+  return typeof error === 'object' && error !== null ? error.code : 0;
 }
 
 function createError(ge: GmailError | string) {
@@ -67,62 +59,90 @@ function createError(ge: GmailError | string) {
   return new Error(ge.error as string);
 }
 
-export class GmailTransport implements Transport {
+interface GoAuth2 {
+  access_token: string;
+  token_type: string;
+  id_token: string;
+  scope: string;
+  expires_in: number;
+}
 
+export class GmailTransport implements Transport {
   public name: string = 'GmailTransport';
   public version: string = 'N/A';
 
   constructor(private options: Options) {}
 
-  private getAccessToken(): Promise<string> {
-    return Requestly.post({
-      protocol: 'https:',
-      hostname: 'www.googleapis.com',
-      path: '/oauth2/v4/token'
-    }, refreshTokenParams(this.options.auth))
-      .then((data: GoAuth2) => data.access_token);
-  }
-
-  private post(message: Buffer, accessToken: string): Promise<any> {
-    return Requestly.postRFC822({
-      protocol: 'https:',
-      hostname: 'www.googleapis.com',
-      path: `/upload/gmail/v1/users/${this.options.userId || 'me'}/messages/send?uploadType=multipart`,
-      headers: {
-        Authorization: `Bearer ${accessToken}`
+  private getAccessToken(): Promise<GoAuth2['access_token']> {
+    return post<GoAuth2>(
+      {
+        protocol: 'https:',
+        hostname: 'www.googleapis.com',
+        path: '/oauth2/v4/token',
+      },
+      refreshTokenParams(this.options.auth),
+    ).then((data) => {
+      if (typeof data === 'string') {
+        return Promise.reject(data);
       }
-    }, message.toString());
-  }
 
-  private sendMail(data: Buffer, accessToken: string, mail: MailMessage, done: DoneCallback): Promise<void> {
-    return this.post(data, accessToken).then((message) => {
-      done(null, {
-        envelope: mail.message.getEnvelope(),
-        messageId: mail.message.messageId(),
-        accessToken: accessToken,
-        message: message
-      });
+      return data.access_token;
     });
   }
 
-  public send(mail: MailMessageWithBcc, done: (err: Error | null, info?: SentMessageInfo) => void): void {
+  private sendMail(message: Buffer, accessToken: string): Promise<unknown> {
+    return postRFC822(
+      {
+        protocol: 'https:',
+        hostname: 'www.googleapis.com',
+        path: `/upload/gmail/v1/users/${
+          this.options.userId || 'me'
+        }/messages/send?uploadType=multipart`,
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      },
+      message.toString(),
+    );
+  }
+
+  public send(mail: MailMessageWithBcc, done: DoneCallback): void {
     mail.message.keepBcc = true;
     mail.message.build((error, data: Buffer) => {
       if (error) return done(error);
+      if (!data) return done(new Error('The email data is corrapted.'));
 
-      this.sendMail(data, this.options.auth.accessToken, mail, done)
-        .catch((e: GmailError) => {
-
-          if (getErrorCode(e.error) === 401 && this.options.auth.refreshToken) {
-            this.getAccessToken().then((accessToken: string) => {
-              this.sendMail(data, accessToken, mail, done)
-                .catch((er: GmailError) => done(createError(er)));
-            }).catch((er: GmailError) => done(createError(er)));
+      this.sendMail(data, this.options.auth.accessToken!).then(
+        (message) => {
+          done(null, {
+            envelope: mail.message.getEnvelope(),
+            messageId: mail.message.messageId(),
+            accessToken: this.options.auth.accessToken!,
+            message: message,
+          });
+        },
+        (err: GmailError) => {
+          if (getErrorCode(err.error) === 401 && this.options.auth.refreshToken) {
+            this.getAccessToken()
+              .then((accessToken: GoAuth2['access_token']) => {
+                this.sendMail(data, accessToken).then(
+                  (message) => {
+                    done(null, {
+                      envelope: mail.message.getEnvelope(),
+                      messageId: mail.message.messageId(),
+                      accessToken: accessToken,
+                      message: message,
+                    });
+                  },
+                  (err: GmailError | string) => done(createError(err)),
+                );
+              })
+              .catch((err: GmailError | string) => done(createError(err)));
+          } else {
+            done(createError(err));
           }
-          else {
-            done(createError(e));
-          }
-      });
+        },
+      );
     });
   }
 }
